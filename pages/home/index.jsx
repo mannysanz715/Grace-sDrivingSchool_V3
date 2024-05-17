33602c9f199739617654b72864b5e3d35d491ef9
MannySanz <mannysleandro@gmail.com>
2024-05-17 16:30:53 -0700

    Setup Landing Page Structure

---
+++ pages/home/index.jsx
@@ -0,0 +1,30 @@
+import './index.css'
+
+function MainHome () {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+}
+
+export default MainHome;
+
+
+function Buttons () {
+  return(
+    <div className="text-white flex flex-col gap-10 mb-[32%] sm:flex-row sm:mb-[10%]">
+      <div className="p-5 rounded-md hover:bg-sky-300 bg-sky-400 text-xl font-bold"><h1>Call to Schedule</h1></div>
+      <div className="p-5 rounded-md hover:bg-yellow-300 bg-yellow-400 text-xl font-bold"><h1>Check Our Rates</h1></div>
+    </div>
+  );
+}
+
+function Banner(){
+  return(
+    <div id="bannerSection" className=" flex flex-col justify-between items-center gap-20">
+      <img src="/logo.png" alt="Grace's Driving School" className="max-sm:w-4/5 max-sm:mt-[25%] max-md:w-3/4 max-md:mt-[10%]  md:w-2/5 md:mt-[3%] lg:mt-[5%] lg:w-2/6 xl:w-1/4 xl:mt-[4%]" />
+      <Buttons />
+    </div>
+  );
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+export default function NavBar () {
+  return ( 
+    <div class="bg-blue-400 bg-opacity-20 h-16 flex flex-row justify-between p-4 items-center">
+      <h1>NavBar</h1>
+      <h1>NavBar</h1>
+  </div> 
+);
+}
